c9e06f7dcf18a1476610c511ca11d2dfa48f651c
Ignat Tolchanov <itolchanov@smprc.ru>
2013-05-21 20:12:35 +0400

    Some parsing works

--- src/Main.hs
+++ src/Main.hs
@@ -1,19 +1,20 @@
 module Main
-    ( main
+    ( main, createStmt
     ) where
 
 
 import System.Environment
 import Data.List
+import Text.ParserCombinators.Parsec
 
 data InputArgs = InputArgs
     {   
         prevVsn :: FilePath,
         currVsn :: FilePath,
         diff    :: Maybe FilePath
-    } deriving(Show)
+    } deriving (Show)
 
---main :: IO ()
+
 main = do
     a <- getArgs
     case parseArgs a of
@@ -34,3 +35,86 @@ parseArgs _ = Nothing
 head' :: [a] -> Maybe a
 head' []     = Nothing
 head' (x:xs) = Just x
+
+
+data SqlColumnType = 
+    SimpleType {
+        typeName     :: String,
+        typeSize     :: Int,
+        typeUnsigned :: Bool}
+    | Enum {values :: [String]}
+    deriving (Show)
+
+data SqlDefaultValue = Null | NotNull | DefaultValue { value :: String} deriving (Show)
+
+data SqlColumn = SqlColumn
+    {
+        colName     :: String,
+        colType     :: SqlColumnType,
+        colDefault  :: SqlDefaultValue
+    } deriving (Show)
+
+
+data SqlTable = SqlTable
+    {
+        tabName    :: String,
+        tabEngine  :: String,
+        tabCharset :: String ,
+        tabColumns :: [SqlColumn]
+    } deriving (Show)
+
+
+space' = many space
+identifier = many (alphaNum <|> char '_')
+
+createStmt :: Parser SqlTable
+createStmt = do 
+    { 
+        space'; string "CREATE"; space'; string "TABLE"; space'; 
+        tableName <- identifier; space';
+        char '(';
+        columns <- columnListStmt;
+        char ')';
+        engineName  <- engineStmt;
+        charsetName <- charsetStmt;
+        return SqlTable {tabName = tableName, tabEngine = engineName, tabCharset = charsetName, tabColumns = columns}
+    }
+
+engineStmt = do 
+    {
+        space'; string "ENGINE"; space'; char '='; space';
+        engineName <- identifier;
+        return engineName
+    }
+
+charsetStmt = do
+    {
+        space'; string "DEFAULT"; space'; string "CHARSET"; space'; char '='; space';
+        charsetName <- identifier;
+        return charsetName
+    }
+
+columnListStmt = sepBy columnStmt (char ',')
+
+
+columnStmt :: Parser SqlColumn
+columnStmt = do
+    {
+        space'; char '`';
+        ident    <- identifier;
+        char '`';
+        ctype    <- columTypeStmt;
+        cdefault <- columnDefaultStmt;
+        return SqlColumn {colName = ident, colType = ctype, colDefault = cdefault}
+    }
+
+
+columTypeStmt = do 
+    {
+        return SimpleType {typeName = "bigint", typeSize = 20, typeUnsigned = True}
+    }
+
+columnDefaultStmt = do
+    {
+        return NotNull
+    }
